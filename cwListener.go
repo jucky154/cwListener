325d15c4a587c9af4e1debe9120d64b6df0e320b
jucky154 <chemistry54@docomo.ne.jp>
2023-06-10 22:07:13 +0900

    realtime analysis

--- cwListener.go
+++ cwListener.go
@@ -1,5 +1,5 @@
 /*
- Copyright (C) 2022 JA1ZLO.
+Copyright (C) 2023 JA1ZLO.
 */
 package main
 
@@ -8,13 +8,11 @@ import (
 	_ "embed"
 	"encoding/binary"
 	"github.com/gen2brain/malgo"
-	"github.com/r9y9/gossp/stft"
-	"github.com/r9y9/gossp/window"
 	"github.com/tadvi/winc"
 	"github.com/tadvi/winc/w32"
-	"math"
-	"unsafe"
+	"github.com/thoas/go-funk"
 	"strings"
+	"unsafe"
 	"zylo/morse"
 	"zylo/reiwa"
 	"zylo/win32"
@@ -24,9 +22,9 @@ const (
 	CWLISTENER_WINDOW = "MainForm.MainMenu.cwListenerWindow"
 	WINDOW_H          = 300
 	WINDOW_W          = 700
-	length_list       = 10
-	recordtime        = 0.3  //リングバッファの時間[s]
-	limit_recordtime  = 60.0 //解析に回せる最大の時間[s]
+	length_list       = 20
+	INTERVAL_MS       = 400
+	LIFE_THRESH       = 2
 )
 
 var (
@@ -45,13 +43,19 @@ var (
 	availabledevices []deviceinfostruct
 	device           *malgo.Device
 	ctx              *malgo.AllocatedContext
+	monitor          morse.Monitor
+	prev_results     map[int]morsedata
+	old_results      []string
 )
+
+type morsedata struct {
+	age	int
+	text	string
+}
 
 type deviceinfostruct struct {
 	devicename string
 	deviceid   unsafe.Pointer
-	maxsample  uint32
-	minsample  uint32
 }
 
 type CWView struct {
@@ -61,29 +65,32 @@ type CWView struct {
 var cwview CWView
 
 type CWItem struct {
-	status       string
-	morseresult1 string
-	morseresult2 string
-	morseresult3 string
+	analysis    string
+	morseresult string
 }
 
 var cwitemarr []CWItem
 
-func (item CWItem) Text() (text []string) {
-	text = append(text, item.status)
-	text = append(text, item.morseresult1)
-	text = append(text, item.morseresult2)
-	text = append(text, item.morseresult3)
+type CWListItem struct {
+	index int
+}
+
+func (item CWListItem) Text() (text []string) {
+	if item.index < len(cwitemarr) {
+		text = append(text, cwitemarr[item.index].analysis)
+		text = append(text, cwitemarr[item.index].morseresult)
+	}
 	return
 }
 
-func (item CWItem) ImageIndex() int {
-	return 0
+func (item CWListItem) ImageIndex() int {
+	return item.index
 }
 
 func init() {
 	reiwa.PluginName = "cwListener"
 	reiwa.OnLaunchEvent = onLaunchEvent
+	reiwa.OnFinishEvent = onFinishEvent
 }
 
 func onLaunchEvent() {
@@ -114,19 +121,19 @@ func onLaunchEvent() {
 	createWindow()
 }
 
+func onFinishEvent() {
+	closeWindow(nil)
+}
+
 func availabledevice() (deviceinfos []deviceinfostruct) {
-	ctx, err := malgo.InitContext(nil, malgo.ContextConfig{}, func(message string) {
-		reiwa.DisplayToast(message)
+	var err error
+	ctx, err = malgo.InitContext(nil, malgo.ContextConfig{}, func(message string) {
+		return
 	})
 
 	if err != nil {
 		reiwa.DisplayToast(err.Error())
 	}
-
-	defer func() {
-		_ = ctx.Uninit()
-		ctx.Free()
-	}()
 
 	infos, err := ctx.Devices(malgo.Capture)
 	if err != nil {
@@ -136,15 +143,13 @@ func availabledevice() (deviceinfos []deviceinfostruct) {
 	deviceinfos = make([]deviceinfostruct, 0)
 
 	for _, info := range infos {
-		full, err := ctx.DeviceInfo(malgo.Capture, info.ID, malgo.Shared)
+		_, err := ctx.DeviceInfo(malgo.Capture, info.ID, malgo.Shared)
 		if err != nil {
 			reiwa.DisplayToast(info.Name() + " is " + err.Error())
 		} else {
 			deviceinfo := deviceinfostruct{
 				devicename: info.Name(),
 				deviceid:   info.ID.Pointer(),
-				maxsample:  full.MaxSampleRate,
-				minsample:  full.MinSampleRate,
 			}
 			deviceinfos = append(deviceinfos, deviceinfo)
 		}
@@ -166,30 +171,29 @@ func createWindow() {
 	//コンボボックス
 	combo = winc.NewComboBox(form)
 	combo.OnSelectedChange().Bind(func(e *winc.Event) {
-		device.Uninit()
-		ctx.Uninit()
+		if device != nil {
+			device.Uninit()
+			device = nil
+		}
 		initdevice()
 	})
 
 	//リスト
 	cwview.list = winc.NewListView(form)
 	cwview.list.EnableEditLabels(false)
-	cwview.list.AddColumn("状況", 100)
-	for i := 0; i < 3; i++ {
-		cwview.list.AddColumn("解析結果", 200)
-	}
+	cwview.list.AddColumn("解析中", 350)
+	cwview.list.AddColumn("結果", 350)
 
 	cwitemarr = make([]CWItem, length_list)
 	for i := 0; i < len(cwitemarr); i++ {
-		cwitemarr[i].status = "-"
-		cwitemarr[i].morseresult1 = "-"
-		cwitemarr[i].morseresult2 = "-"
-		cwitemarr[i].morseresult3 = "-"
+		cwitemarr[i].analysis = "-"
+		cwitemarr[i].morseresult = "-"
 	}
 
-	for _, val := range cwitemarr {
-		cwview.list.AddItem(val)
-
+	for idx, _ := range cwitemarr {
+		cwview.list.AddItem(CWListItem{
+			index: idx,
+		})
 	}
 
 	//dock
@@ -204,48 +208,25 @@ func createWindow() {
 
 func closeWindow(arg *winc.Event) {
 	defer reiwa.DisplayPanic()
-	device.Uninit()
-	ctx.Uninit()
-
+	if device != nil {
+		device.Uninit()
+		device = nil
+	}
 	form.Hide()
 }
 
-//この変数はモールス解析用（音声入力装置によって値が変わるので、下で代入する）
-var decoder morse.Decoder
-
-var monitor morse.Monitor
-
-var before_finish bool
-
-var prev_texts []string
-
-func status_bool(before_finish, finish bool) (result string) {
-	switch {
-	case before_finish == true && finish == true :
-		result = "ノイズのみ"
-	case before_finish == false && finish == true :
-		result = "確定"
-	default :
-		result = "解析中"
-	}
-	return
-}
-
-func correct_string(prev_text string , latest_text string, finish bool) (shown_text string) {
-	for i := len(prev_text); i>= 1; i-- {
+func correct_string(prev_text string, latest_text string) (shown_text string) {
+	for i := len(prev_text); i >= 1; i-- {
 		if strings.HasPrefix(latest_text, prev_text[:i]) {
 			shown_text = prev_text[:i]
 			break
 		}
 	}
 
-	if shown_text == ""{
+	if shown_text == "" {
 		shown_text = "-"
 	}
 
-	if finish {
-		shown_text = latest_text
-	}
 	return
 }
 
@@ -256,156 +237,121 @@ func decode_main(signal []float64) {
 	if !form.Visible() {
 		return
 	}
-	
-	decode_result := monitor.Read(signal)
 
-	finish := true
-	morse_texts := make([]string, 0)
-	for _, message := range decode_result {
-		morse_texts = append(morse_texts, morse.CodeToText(message.Code)) 
-		if !message.Finish() {
-			finish = false
+	decode_results := monitor.Read(signal)
+
+	//現在の列に表示するためのリストを作成
+	now_results := make([]string, length_list, length_list)
+	for i := 0; i < length_list; i++ {
+		now_results[i] = "-"
+	}
+	f_int := 0
+
+	prev_map := prev_results
+
+	//過去に解析したものと一致するかの処理
+	for f, prev_text := range prev_map {
+		miss := true
+		for _, m := range decode_results {
+			//前回の解析において存在し、音が続いているものは前回と比較し確定した文字列を調査する
+			if f == m.Freq && m.Life >= LIFE_THRESH {
+				miss = false
+				text := morse.CodeToText(m.Code)
+				if f_int < length_list {
+					now_results[f_int] =  correct_string(prev_text.text, text)
+					f_int += 1
+				}
+			}
+		}
+
+		//前回の解析において存在するが、音が続いていないものは右に移し、mapから削除
+		if result_data, ok := prev_results[f]; ok && miss {
+			if result_data.text != "-" && result_data.text != "?" && result_data.age > LIFE_THRESH{
+				for i := 0; i < length_list-1; i++ {
+					old_results[length_list-1-i] = old_results[length_list-2-i]
+				}
+				old_results[0] = result_data.text
+			}
+			delete(prev_results, f)
 		}
 	}
 
-	//まず、空の結果を最初に入れて置き、結果があるところは後で修正
-	cwitems := CWItem{
-		status:       status_bool(before_finish, finish),
-		morseresult1: "-",
-		morseresult2: "-",
-		morseresult3: "-",
-	}
-
-	for i := 0; i < int(math.Min(float64(len(decode_result)), float64(3))); i++ {
-		latest_text := morse_texts[i]
-		switch i + 1 {
-		case 1:
-			cwitems.morseresult1 = correct_string(prev_texts[i], latest_text, finish)
-		case 2:
-			cwitems.morseresult2 = correct_string(prev_texts[i], latest_text, finish)
-		case 3:
-			cwitems.morseresult3 = correct_string(prev_texts[i], latest_text, finish)
+	//次回の解析のために確定していない文字列をmapに入れる
+	for _, m := range decode_results {
+		if m.Life >= LIFE_THRESH {
+			if _, ok := prev_results[m.Freq]; ok {
+				morseage := prev_results[m.Freq].age 
+				prev_results[m.Freq] = morsedata{
+						age : morseage +1 ,
+						text : morse.CodeToText(m.Code),
+				}
+			} else {
+				prev_results[m.Freq] = morsedata{
+						age : 0,
+						text : morse.CodeToText(m.Code),
+				}
+			}
 		}
-		prev_texts[i] = latest_text
 	}
 
-	cwitemarr[length_list-1] = cwitems
-
-	//リストの更新（画面ないのにやったらまずそうなので）
+	//リストの更新
+	//フォームが存在するならば表示
 	if form.Visible() {
-		cwview.list.DeleteAllItems()
-		for _, val := range cwitemarr {
-			cwview.list.AddItem(val)
+		for i := 0; i < length_list; i++ {
+			cwitemarr[i].analysis = now_results[i]
+			cwitemarr[i].morseresult = old_results[i]
+			cwview.list.UpdateItem(cwview.list.Items()[i])
 		}
-	}
-
-	switch {
-	case before_finish == false && finish == true :
-		before_finish = finish
-		for i, val := range cwitemarr[1:] {
-			cwitemarr[i] = val
-		}
-	default :
-		before_finish = finish
 	}
 	return
 }
 
-func samplingrate(maxsample uint32, minsample uint32) (sample uint32) {
-	sample = uint32(44100)
-	if int(maxsample) < 44100 {
-		sample = maxsample
-	}
-	if int(minsample) > 44100 {
-		sample = minsample
+func DeviceConfig(deviceID unsafe.Pointer) (cfg malgo.DeviceConfig) {
+	cfg = malgo.DefaultDeviceConfig(malgo.Capture)
+	cfg.Capture.Format = malgo.FormatS32
+	cfg.Capture.Channels = 1
+	cfg.Capture.DeviceID = deviceID
+	cfg.Alsa.NoMMap = 1
+	cfg.PeriodSizeInMilliseconds = INTERVAL_MS
+	return
+}
+
+func readSignedInt(signal []byte) (result []float64) {
+	for _, b := range funk.Chunk(signal, 4).([][]byte) {
+		v := binary.LittleEndian.Uint32(b)
+		result = append(result, float64(int32(v)))
 	}
 	return
 }
 
 func initdevice() {
-	var err error
 	machinenum := combo.SelectedItem()
-	maxsample := availabledevices[machinenum].maxsample
-	minsample := availabledevices[machinenum].minsample
-	rate_sound := samplingrate(maxsample, minsample)
-	ctx, err = malgo.InitContext(nil, malgo.ContextConfig{}, func(message string) {
-		reiwa.DisplayToast(message)
-		return
-	})
+	deviceConfig := DeviceConfig(availabledevices[machinenum].deviceid)
 
-	defer func() {
-		_ = ctx.Uninit()
-		ctx.Free()
-	}()
-
-	if err != nil {
-		reiwa.DisplayModal("機器の初期化中に問題が発生しました。音声機器の接続を確認し、プラグインウィンドウを開きなおしてください")
-		reiwa.DisplayToast(err.Error())
-		return
-
+	//前の周波数を記録しておく変数
+	prev_results = make(map[int]morsedata)
+	old_results = make([]string, length_list, length_list)
+	for i := 0; i < length_list; i++ {
+		old_results[i] = "-"
 	}
 
-	deviceConfig := malgo.DefaultDeviceConfig(malgo.Duplex)
-	deviceConfig.Capture.Format = malgo.FormatS32
-	deviceConfig.Capture.Channels = 1
-	deviceConfig.SampleRate = rate_sound
-	deviceConfig.Capture.DeviceID = availabledevices[machinenum].deviceid
-	deviceConfig.Alsa.NoMMap = 1
-	deviceConfig.PeriodSizeInMilliseconds = uint32(1000)
-
-	length_buffer := int(float64(rate_sound) * float64(recordtime))
-
-	sound_buffer := make([]float64, 0)
-
-	//decodeに必要な情報をここで入れる
-	decoder = morse.Decoder{
-		Thre: 0.1,
-		Iter: 10,
-		Bias: 10,
-		Gain : 2.0,
-		STFT: &stft.STFT{
-			FrameShift: int(rate_sound) / 50,
-			FrameLen:   2048,
-			Window:     window.CreateHanning(2048),
-		},
-	}
-
-	monitor = morse.Monitor{
-		MaxHold: int(rate_sound) * 60,
-		Decoder: decoder,
-	}
-
-	before_finish = true 
-
-	prev_texts = []string {"-", "-", "-"}
-
-	buffer_cnt := 0
-
-	onRecvFrames := func(pSample2, pSample []byte, framecount uint32) {
-		Signalint := make([]int32, framecount)
-		buffer := bytes.NewReader(pSample)
-		binary.Read(buffer, binary.LittleEndian, &Signalint)
-		for _, val := range Signalint {
-			sound_buffer = append(sound_buffer, float64(val))
-			buffer_cnt += 1
-			if buffer_cnt == length_buffer {
-				go decode_main(sound_buffer)
-				sound_buffer = make([]float64, 0)
-				buffer_cnt = 0
-			}
-		}
+	onRecvFrames := func(out, in []byte, framecount uint32) {
+		go decode_main(readSignedInt(in))
 	}
 
 	captureCallbacks := malgo.DeviceCallbacks{
 		Data: onRecvFrames,
 	}
 
+	var err error
 	device, err = malgo.InitDevice(ctx.Context, deviceConfig, captureCallbacks)
 	if err != nil {
 		reiwa.DisplayModal("機器の初期化中に問題が発生しました。音声機器の接続を確認し、プラグインウィンドウを開きなおしてください")
 		reiwa.DisplayToast(err.Error())
 		return
 	}
+
+	monitor = morse.DefaultMonitor(int(device.SampleRate()))
 
 	err = device.Start()
 	if err != nil {
